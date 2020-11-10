6e22c563b0e88f02f5ced46890b6690c013da61a
Sandra Aguayo Nuñez <sandraguay31@gmail.com>
2020-11-09 22:47:38 -0600

    12 octubre Init

--- public/javascripts/slider.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('#slider div:gt(0)').hide();
-    setInterval(function(){
-        $('#slider div:first-child').fadeOut(1000)
-        .next('div').fadeIn(1000)
-        .end().appendTo('#slider');
-    },4000);
-})
--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,34 @@
 var express = require('express');
 var router = express.Router();
+//var nombres = ['María','Carmen','Angélica','Valentina','Yolanda','Mauricio','Luis','Pedro','Gustavo','Fabián'];
 
-
+/* GET home page. Página Principal*/
 router.get('/', function (req, res, next) {
   res.render('index', { page: 'Home', menuId: 'home' });
-});
-router.get('/signup', function (req, res, next) {
-  res.render('pages/signup', { page: 'Signup', menuId: 'signup' });
-});
-router.get('/login', function (req, res, next) {
-  res.render('pages/login', { page: 'Login', menuId: 'login' });
 });
 
 router.get('/ubicacion', function (req, res, next) {
   res.render('pages/ubicacion', { page: 'Ubicacion', menuId: 'ubicacion' });
 });
 
-router.get('/redessociales', function (req, res, next) {
-  res.render('pages/redessociales', { page: 'Redes sociales', menuId: 'redessociales' });
-});
-
-router.get('/ecommerce', function (req, res, next) {
-  res.render('pages/ecommerce', { page: 'Ecommerce', menuId: 'ecommerce' });
-});
-
-router.get('/streaming', function (req, res, next) {
-  res.render('pages/streaming', { page: 'Streaming', menuId: 'streaming' });
-});
-
-router.get('/otra', function (req, res, next) {
-  res.render('pages/otra', { page: 'Otra', menuId: 'otra' });
-});
-
 router.get('/acercade', function (req, res, next) {
-  res.render('pages/acercade', { page: 'Acercade', menuId: 'acercade' });
+  res.render('acercade', { page: 'Acercade', menuId: 'acercade' });
+});
+
+
+//Método POST
+router.post('/', function (req, res) {
+  res.send('Tengo una petición con POST');
+});
+
+//Método PUT
+router.put('/greeting', function (req, res) {
+  res.send('Te doy un saludo con Greeting');
+});
+
+//Método DELETE
+router.delete('/hello', function (req, res) {
+  res.send('Te doy un saludo con DELETE');
 });
 
 module.exports = router;
