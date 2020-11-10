123b7e2510e307277c4eed1ab023beb3614d3218
Sandra Aguayo Nuñez <sandraguay31@gmail.com>
2020-11-10 01:33:46 -0600

    12 octubre Init

---
+++ routes/acercade.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. Página Principal*/
+//router.get('/', function(req, res, next) {
+//  res.render('acercade', { title: 'Acerca de '});
+//});
+
+module.exports = router;
